47c66e3a48bc6d7d8e8d6460e529c70dc7121e7f
Jake Varley <jakevarley@gmail.com>
2016-12-19 17:47:59 -0500

    reorganizing as packaage

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+#from distutils.core import setup,find_packages
+
+setup(name='binvox-rw',
+      version='1.0',
+      description='read and write binvox files in python',
+      author='Jake Varley',
+      author_email='jakevarley@gmail.com',
+      url='https://github.com/CURG/binvox-rw-py',
+      packages=find_packages(),
+     )
